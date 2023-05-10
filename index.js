38e3d9949d51c3e836f3f96656cc09c1b5601a27
akshanshchauhan17 <akshanshchauhan47@gmail.com>
2023-05-10 14:09:04 +0530

    ksdjlfkjdsjfjdssdkfklj

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const app = require("./home/index");
 const server = http.createServer(app);
 const port = process.env.PORT || 1000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (res.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, PATCH, DELETE, PUT, GET');
+        return res.status(200).json({});
+        next();
+    }
+})
+
 server.listen(port, () => {
     console.log("SERVER STARTED SUCCESSFULLY!!! AT " + port + " PORT");
 });
