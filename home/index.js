afe76fb9da4332351a4d7ecd32e564c0cc83a7c7
akshanshchauhan17 <akshanshchauhan47@gmail.com>
2023-05-10 22:24:44 +0530

    lklksdhdfhdhhhfhhf

--- home/index.js
+++ home/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (res.method === 'OPTIONS') {
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'POST, PATCH, DELETE, PUT, GET');
         return res.status(200).json({});
     }
